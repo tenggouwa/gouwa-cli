ea55d29ea05bae1070906654cf6aa4a634f5e0e7
tenggouwa <17681875268@163.com>
2023-08-11 16:14:01 +0800

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const a = 1
+const b = 2
+const add = (a: number, b: number) => {
+  return a + b
+}
+export default add(a, b)
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    target: 'modules',
+    minify: true,
+    rollupOptions: {
+      input: ['src/index.ts'],
+      output: [
+        {
+          format: 'es',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          dir: 'es',
+          preserveModulesRoot: 'src'
+        },
+        {
+          format: 'cjs',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          dir: 'lib',
+          preserveModulesRoot: 'src'
+        }
+      ]
+    },
+    lib: {
+      entry: './lib/main.ts',
+    }
+  }
+})
